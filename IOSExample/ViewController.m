6a00e93646f1880e18657bb98cacadf8eb3540bd
bobo <jqbo84@163.com>
2017-12-29 14:30:13 +0800

    修改UIButton样式效果

--- IOSExample/ViewController.m
+++ IOSExample/ViewController.m
@@ -30,8 +30,13 @@
 
 - (UIButton *)addPassValueButtonView {
     if (_passValueButton == nil) {
-        _passValueButton = [[UIButton alloc] initWithFrame:CGRectMake(10, 50, 350, 40)];
+        _passValueButton = [[UIButton alloc] initWithFrame:CGRectMake(10, 50, mScreenWidth - 10 * 2, 40)];
         _passValueButton.backgroundColor = [UIColor redColor];
+        _passValueButton.layer.cornerRadius = 5;
+        _passValueButton.layer.borderWidth = 1.0;
+        _passValueButton.layer.borderColor = [UIColorUtils colorWithHexString:@"#C4575B"].CGColor;
+        //点击时高亮
+        _passValueButton.showsTouchWhenHighlighted=YES;
         [_passValueButton setTitle:@"测试6种传值的方法" forState:UIControlStateNormal];
         [_passValueButton setTitleColor:[UIColor whiteColor] forState:UIControlStateNormal];
         [_passValueButton addTarget:self action:@selector(passValueClickListener) forControlEvents:UIControlEventTouchUpInside];
@@ -41,8 +46,14 @@
 
 - (UIButton *)addEnterArrayButtonView {
     if(_enterArrayButton == nil){
-        _enterArrayButton = [[UIButton alloc] initWithFrame:CGRectMake(10, 100, 350, 40)];
+        NSInteger indexY = _passValueButton.frame.origin.y + _passValueButton.frame.size.height + 10;
+        _enterArrayButton = [[UIButton alloc] initWithFrame:CGRectMake(10, indexY, mScreenWidth - 10 * 2, 40)];
         _enterArrayButton.backgroundColor = [UIColor redColor];
+        _enterArrayButton.layer.cornerRadius = 5;
+        _enterArrayButton.layer.borderWidth = 1.0;
+        _enterArrayButton.layer.borderColor = [UIColorUtils colorWithHexString:@"#C4575B"].CGColor;
+        //点击时高亮
+        _enterArrayButton.showsTouchWhenHighlighted=YES;
         [_enterArrayButton setTitle:@"学习NSMutableString常用用法" forState:UIControlStateNormal];
         [_enterArrayButton setTitleColor:[UIColor whiteColor] forState:UIControlStateNormal];
         [_enterArrayButton addTarget:self action:@selector(enterArrayClickListener) forControlEvents:UIControlEventTouchUpInside];
@@ -52,11 +63,14 @@
 
 - (UIButton *)addHttpButtonView {
     if(_httpButton == nil){
-    _httpButton = [[UIButton alloc]initWithFrame:CGRectMake(10, 150, mScreenWidth - 10 * 2, 40)];
+        NSInteger indexY = _enterArrayButton.frame.origin.y + _enterArrayButton.frame.size.height;
+        _httpButton = [[UIButton alloc]initWithFrame:CGRectMake(10, indexY + 10, mScreenWidth - 10 * 2, 40)];
         _httpButton.backgroundColor = [UIColor redColor];
         _httpButton.layer.cornerRadius = 5;
         _httpButton.layer.borderWidth = 1.0;
         _httpButton.layer.borderColor = [UIColorUtils colorWithHexString:@"#C4575B"].CGColor;
+        //点击时高亮
+        _httpButton.showsTouchWhenHighlighted=YES;
         [_httpButton setTitle:@"学习网络请求常用用法" forState:UIControlStateNormal];
         [_httpButton setTitleColor:[UIColor whiteColor] forState:UIControlStateNormal];
         [_httpButton addTarget:self action:@selector(httpClickListener) forControlEvents:UIControlEventTouchUpInside];
