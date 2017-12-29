c0853e4b5b9cae4699033fb6745d83bb2137c618
bobo <jqbo84@163.com>
2017-12-29 16:49:55 +0800

    添加POST方式请求接口

--- IOSExample/net/HttpViewController.m
+++ IOSExample/net/HttpViewController.m
@@ -15,7 +15,8 @@
 @interface HttpViewController ()
 
 @property (nonatomic, strong) UIButton *backButton;
-@property (nonatomic, strong) UIButton *callButton;
+@property (nonatomic, strong) UIButton *getButton;
+@property (nonatomic, strong) UIButton *postButton;
 @property (nonatomic, strong) UILabel *resultLabel;
 
 @end
@@ -29,6 +30,8 @@
         _backButton.layer.cornerRadius = 4;
         _backButton.layer.borderWidth = 1.0;
         _backButton.layer.borderColor = [UIColorUtils colorWithHexString:@"#C4575B"].CGColor;
+        //点击时高亮
+        _backButton.showsTouchWhenHighlighted = YES;
         [_backButton setTitle:@"返回" forState:UIControlStateNormal];
         [_backButton setTitleColor:[UIColor whiteColor] forState:UIControlStateNormal];
         [_backButton addTarget:self action:@selector(backClick) forControlEvents:UIControlEventTouchUpInside];
@@ -36,26 +39,46 @@
     return _backButton;
 }
 
-- (UIButton *)addCallButtonView {
-    if(_callButton == nil){
-        _callButton = [[UIButton alloc]initWithFrame:CGRectMake(50, _backButton.frame.origin.y + _backButton.frame.size.height + 10, mScreenWidth - 50 * 2, 40)];
-        _callButton.backgroundColor = [UIColor redColor];
-        _callButton.layer.cornerRadius = 4;
-        _callButton.layer.borderWidth = 1.0;
-        _callButton.layer.borderColor = [UIColorUtils colorWithHexString:@"#C4575B"].CGColor;
-        [_callButton setTitle:@"Get方式请求接口" forState:UIControlStateNormal];
-        [_callButton setTitleColor:[UIColor whiteColor] forState:UIControlStateNormal];
-        [_callButton addTarget:self action:@selector(loadGetRequest) forControlEvents:UIControlEventTouchUpInside];
+- (UIButton *)addGetButtonView {
+    if(_getButton == nil){
+        _getButton = [[UIButton alloc]initWithFrame:CGRectMake(20, _backButton.frame.origin.y + _backButton.frame.size.height + 10, (mScreenWidth - 20 * 2) / 2 - 10, 40)];
+        _getButton.backgroundColor = [UIColor redColor];
+        _getButton.layer.cornerRadius = 4;
+        _getButton.layer.borderWidth = 1.0;
+        _getButton.layer.borderColor = [UIColorUtils colorWithHexString:@"#C4575B"].CGColor;
+        //点击时高亮
+        _getButton.showsTouchWhenHighlighted = YES;
+        [_getButton setTitle:@"Get方式请求接口" forState:UIControlStateNormal];
+        [_getButton setTitleColor:[UIColor whiteColor] forState:UIControlStateNormal];
+        [_getButton.titleLabel setFont:[UIFont fontWithName:@"Helvetica-Bold" size:14.0f]];
+        [_getButton addTarget:self action:@selector(loadGetRequest) forControlEvents:UIControlEventTouchUpInside];
     }
-    return _callButton;
+    return _getButton;
+}
+
+- (UIButton *)addPostButtonView {
+    if(_postButton == nil){
+        _postButton = [[UIButton alloc]initWithFrame:CGRectMake(mScreenWidth / 2 + 10, _backButton.frame.origin.y + _backButton.frame.size.height + 10, (mScreenWidth - 20 * 2) / 2 - 10, 40)];
+        _postButton.backgroundColor = [UIColor redColor];
+        _postButton.layer.cornerRadius = 4;
+        _postButton.layer.borderWidth = 1.0;
+        _postButton.layer.borderColor = [UIColorUtils colorWithHexString:@"#C4575B"].CGColor;
+        _postButton.titleLabel.font = [UIFont systemFontOfSize: 14];
+        //点击时高亮
+        _postButton.showsTouchWhenHighlighted = YES;
+        [_postButton setTitle:@"POST方式请求接口" forState:UIControlStateNormal];
+        [_postButton setTitleColor:[UIColor whiteColor] forState:UIControlStateNormal];
+        [_postButton addTarget:self action:@selector(loadPostRequest) forControlEvents:UIControlEventTouchUpInside];
+    }
+    return _postButton;
 }
 
 - (UILabel *)addLabelResultView {
     if(_resultLabel == nil){
-        _resultLabel = [[UILabel alloc]initWithFrame:CGRectMake(10, _callButton.frame.origin.y + _callButton.frame.size.height + 10, mScreenWidth - 10 * 2, mScreenHeight - (_callButton.frame.origin.y + _callButton.frame.size.height + 10))];
+        _resultLabel = [[UILabel alloc]initWithFrame:CGRectMake(10, _getButton.frame.origin.y + _getButton.frame.size.height + 10, mScreenWidth - 10 * 2, mScreenHeight - (_getButton.frame.origin.y + _getButton.frame.size.height + 10))];
         _resultLabel.backgroundColor = [UIColor whiteColor];
         _resultLabel.textColor = [UIColor blackColor];
-        _resultLabel.font = [UIFont systemFontOfSize:20];
+        _resultLabel.font = [UIFont systemFontOfSize: 16];
         _resultLabel.numberOfLines = 0;
         _resultLabel.lineBreakMode = NSLineBreakByWordWrapping;
     }
@@ -111,6 +134,96 @@
     [dataTask resume];
 }
 
+- (void)loadPostRequest {
+    NSDictionary *headers = @{ @"content-type": @"application/x-www-form-urlencoded",
+                               @"cache-control": @"no-cache"};
+    
+    NSURL *url = [NSURL URLWithString:@"http://www.justonetech.com:8090/androidKq/getProjects.do"];
+    
+//    NSString *param = @"data=%7B%22token%22%3A%20%22lWQoE0jzlvRrvj2KEcsqL9PxsoGYw6mznrQCB6ai%2Fm%2BjFDzkaUaeehu8hxZ6SYqLMjR12m8NQWEX%24%24%24%40%40%40LWtK2e5lzRNtE8vW4qBP3QwiXkTD1R4%3D%22%7D";
+    
+    NSDictionary *paramObj = @{@"token":@"lWQoE0jzlvRrvj2KEcsqL9PxsoGYw6mznrQCB6ai/m+jFDzkaUaeehu8hxZ6SYqLMjR12m8NQWEX$$$@@@LWtK2e5lzRNtE8vW4qBP3QwiXkTD1R4="};
+    NSLog(@"paramObj = %@", paramObj);
+    
+    NSData *jsonData = [NSJSONSerialization dataWithJSONObject:paramObj options:NSJSONWritingPrettyPrinted error:nil];
+    NSLog(@"jsonData = %@", jsonData);
+    NSString *jsonString = [[NSString alloc] initWithData:jsonData encoding:NSUTF8StringEncoding];
+    NSLog(@"jsonString = %@", jsonString);
+    
+//    NSString *encodedValue = [jsonString stringByAddingPercentEncodingWithAllowedCharacters:[NSCharacterSet URLQueryAllowedCharacterSet]];
+//    NSLog(@"encodedValue = %@", encodedValue);
+    
+    //将参数做编码处理
+    jsonString = [self encodeParamString:jsonString];
+    
+    NSString *param = [NSString stringWithFormat:@"data=%@", jsonString];
+    NSLog(@"param = %@", param);
+    
+    NSMutableData *postData = [[NSMutableData alloc] initWithData:[param dataUsingEncoding:NSUTF8StringEncoding]];
+    NSLog(@"postData = %@", postData);
+    
+    NSMutableURLRequest *request = [NSMutableURLRequest requestWithURL:url cachePolicy:NSURLRequestUseProtocolCachePolicy timeoutInterval:10.0];
+    [request setHTTPMethod:@"POST"];
+    [request setAllHTTPHeaderFields:headers];
+    [request setHTTPBody:postData];
+    
+    NSURLSession *session = [NSURLSession sharedSession];
+    NSURLSessionDataTask *dataTask = [session dataTaskWithRequest:request
+                                                completionHandler:^(NSData *data, NSURLResponse *response, NSError *error) {
+                                                    if (error) {
+                                                        NSLog(@"%@", error);
+                                                    } else {
+                                                        NSHTTPURLResponse *httpResponse = (NSHTTPURLResponse *) response;
+                                                        NSLog(@"%@", httpResponse);
+                                                        
+                                                        id obj = [NSJSONSerialization JSONObjectWithData:data options:0 error:nil];
+                                                        NSLog(@"result = %@", obj);
+                                                        
+                                                        if([obj isKindOfClass:[NSDictionary class]]){
+                                                            NSMutableDictionary *datas = [(NSMutableDictionary *)obj objectForKey:@"data"];
+                                                            NSMutableString *mutableString = [[NSMutableString alloc]initWithCapacity:[datas count]];
+                                                            for (id project in datas) {
+                                                                NSString *projectName = [(NSDictionary *)project objectForKey:@"projectName"];
+                                                                [mutableString appendString:projectName];
+                                                                [mutableString appendString:@"\n"];
+                                                            }
+                                                            // 通知主线程刷新，UI更新操作必须在主线程中执行,常嵌套在其他线程中，如下
+                                                            dispatch_async(dispatch_get_main_queue(), ^{
+                                                                //回调或者说是通知主线程刷新，
+                                                                [self updateLabelText:mutableString];
+                                                            });
+                                                        }
+                                                    }
+                                                }];
+    [dataTask resume];
+}
+
+- (NSString *)encodeParamString: (NSString *) paramString {
+    NSInteger strLength = 0;
+    NSMutableString *mutableUrlStr = [paramString mutableCopy];
+    NSLog(@" mutableUrlStr : %@", mutableUrlStr );
+    
+    strLength = [mutableUrlStr length];
+    [mutableUrlStr replaceOccurrencesOfString:@"{" withString:@"%7B" options:NSCaseInsensitiveSearch range:NSMakeRange(0, strLength)];
+    strLength = [mutableUrlStr length];
+    [mutableUrlStr replaceOccurrencesOfString:@"}" withString:@"%7D" options:NSCaseInsensitiveSearch range:NSMakeRange(0, strLength)];
+    strLength = [mutableUrlStr length];
+    [mutableUrlStr replaceOccurrencesOfString:@"\"" withString:@"%22" options:NSCaseInsensitiveSearch range:NSMakeRange(0, strLength)];
+    strLength = [mutableUrlStr length];
+    [mutableUrlStr replaceOccurrencesOfString:@"+" withString:@"%2B" options:NSCaseInsensitiveSearch range:NSMakeRange(0, strLength)];
+    strLength = [mutableUrlStr length];
+    [mutableUrlStr replaceOccurrencesOfString:@"=" withString:@"%3D" options:NSCaseInsensitiveSearch range:NSMakeRange(0, strLength)];
+    strLength = [mutableUrlStr length];
+    [mutableUrlStr replaceOccurrencesOfString:@":" withString:@"%3A" options:NSCaseInsensitiveSearch range:NSMakeRange(0, strLength)];
+    strLength = [mutableUrlStr length];
+    [mutableUrlStr replaceOccurrencesOfString:@"/" withString:@"%2F" options:NSCaseInsensitiveSearch range:NSMakeRange(0, strLength)];
+    strLength = [mutableUrlStr length];
+    [mutableUrlStr replaceOccurrencesOfString:@" " withString:@"" options:NSCaseInsensitiveSearch range:NSMakeRange(0, strLength)];
+    NSLog(@" mutableUrlStr : %@", mutableUrlStr );
+    
+    return mutableUrlStr;
+}
+
 - (void)updateLabelText: (NSString *)text {
     [_resultLabel setText:text];
 }
@@ -141,7 +254,8 @@
     self.view.backgroundColor = [UIColor whiteColor];
     
     [self.view addSubview:self.addBackButtonView];
-    [self.view addSubview:self.addCallButtonView];
+    [self.view addSubview:self.addGetButtonView];
+    [self.view addSubview:self.addPostButtonView];
     [self.view addSubview:self.addLabelResultView];
 }
 
--- README.md
+++ README.md
@@ -1,7 +1,7 @@
 # iOS开发学习笔记
 
 
-###1、学习iOS中6种传值的方法
+### 1、学习iOS中6种传值的方法
 
 	1.属性传值
 	2.单例传值
@@ -9,3 +9,17 @@
 	4.代理传值
 	5.block传值
 	6.通知传值（NSNotification）
+	
+### 2、学习iOS中的字符串类型
+
+	1、NSMutableString
+	2、NSRange
+	3、NSArray
+	4、NSString
+	
+### 3、学习网络请求基本方法
+
+	1、GET方法
+	2、POST方法
+	3、自定义UIColorUtils工具类
+	4、UIImageView加载工程本地图片显示
